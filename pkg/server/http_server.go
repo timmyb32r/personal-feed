14a9d9be941a171dad3c966903ca9a660d2e8e30
Timofey Brunko <timmyb32r@yandex-team.ru>
2023-05-21 12:48:24 +0300

    2023-05-21--fix-favicon

--- pkg/server/http_server.go
+++ pkg/server/http_server.go
@@ -2,8 +2,6 @@ package server
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
 	"log"
@@ -11,8 +9,6 @@ import (
 	"os"
 	"os/signal"
 	"personal-feed/pkg/config"
-	"personal-feed/pkg/repo"
-	"strings"
 	"sync"
 	"syscall"
 	"time"
@@ -40,8 +36,14 @@ func NewHTTPServer(config *config.Config, logger *logrus.Logger) *HTTPServer {
 
 	router := mux.NewRouter()
 
-	router.HandleFunc("/", s.RootHandler)
-	router.PathPrefix("/index.").Handler(newStaticHandler())
+	handler := newStaticHandler()
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		handler.ServeHTTPURI(w, "/index.html")
+	})
+	router.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
+		handler.ServeHTTPURI(w, "/favicon.ico")
+	})
+	router.PathPrefix("/index.").Handler(handler)
 
 	s.httpServer.Handler = router
 
@@ -82,34 +84,34 @@ func (s *HTTPServer) shutdown() {
 	})
 }
 
-func (s *HTTPServer) RootHandler(w http.ResponseWriter, r *http.Request) {
-	repoClient, err := repo.NewRepo(r.Context(), s.config.Repo, s.logger)
-	if err != nil {
-		_, _ = w.Write([]byte(fmt.Sprintf("HTTPServer::RootHandler::error0::%s", err.Error())))
-		return
-	}
-
-	tx, err := repoClient.NewTx(r.Context())
-	if err != nil {
-		_, _ = w.Write([]byte(fmt.Sprintf("HTTPServer::RootHandler::error1::%s", err.Error())))
-		return
-	}
-	defer tx.Rollback(r.Context())
-
-	nodes, err := repoClient.TestExtractAllTreeNodes(tx, r.Context())
-	if err != nil {
-		_, _ = w.Write([]byte(fmt.Sprintf("HTTPServer::RootHandler::error2::%s", err.Error())))
-		return
-	}
-
-	buf := []string{"RESULT:"}
-	for _, el := range nodes {
-		elArr, _ := json.Marshal(el)
-		buf = append(buf, string(elArr))
-	}
-	result := strings.Join(buf, "\n")
-	_, _ = w.Write([]byte(result))
-}
+//func (s *HTTPServer) RootHandler(w http.ResponseWriter, r *http.Request) {
+//	repoClient, err := repo.NewRepo(r.Context(), s.config.Repo, s.logger)
+//	if err != nil {
+//		_, _ = w.Write([]byte(fmt.Sprintf("HTTPServer::RootHandler::error0::%s", err.Error())))
+//		return
+//	}
+//
+//	tx, err := repoClient.NewTx(r.Context())
+//	if err != nil {
+//		_, _ = w.Write([]byte(fmt.Sprintf("HTTPServer::RootHandler::error1::%s", err.Error())))
+//		return
+//	}
+//	defer tx.Rollback(r.Context())
+//
+//	nodes, err := repoClient.TestExtractAllTreeNodes(tx, r.Context())
+//	if err != nil {
+//		_, _ = w.Write([]byte(fmt.Sprintf("HTTPServer::RootHandler::error2::%s", err.Error())))
+//		return
+//	}
+//
+//	buf := []string{"RESULT:"}
+//	for _, el := range nodes {
+//		elArr, _ := json.Marshal(el)
+//		buf = append(buf, string(elArr))
+//	}
+//	result := strings.Join(buf, "\n")
+//	_, _ = w.Write([]byte(result))
+//}
 
 func (s *HTTPServer) DistHandler(w http.ResponseWriter, r *http.Request) {
 
--- pkg/server/static_handler.go
+++ pkg/server/static_handler.go
@@ -4,6 +4,7 @@ import (
 	"embed"
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 //go:embed dist
@@ -12,15 +13,22 @@ var distFiles embed.FS
 type staticHandler struct {
 }
 
-func (h staticHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	pathInDist := "dist" + r.RequestURI
+func (h staticHandler) ServeHTTPURI(w http.ResponseWriter, uri string) {
+	pathInDist := "dist" + uri
 	buf, err := distFiles.ReadFile(pathInDist)
 	if err != nil {
 		_, _ = w.Write([]byte(fmt.Sprintf("staticHandler::ServeHTTP::error1::%s", err.Error())))
 		w.WriteHeader(404)
 		return
 	}
+	if strings.HasSuffix(pathInDist, ".js") {
+		w.Header().Add("Content-Type", "text/javascript") // https://stackoverflow.com/a/9664327
+	}
 	_, _ = w.Write(buf)
+}
+
+func (h staticHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	h.ServeHTTPURI(w, r.RequestURI)
 }
 
 func newStaticHandler() *staticHandler {
