7fd71b4fd051b888e8e83538ab6cd8df89f8c5cc
Timofey Brunko <timmyb32r@yandex-team.ru>
2023-05-22 23:49:34 +0300

    2023-05-22--cure-loop-front--added-allow-cors-flag

--- front/src/services/FeedService.ts
+++ front/src/services/FeedService.ts
@@ -4,71 +4,25 @@ import { Feed, FeedHead } from "../models/Feed"
 import { FeedEvent } from "../models/FeedEvent"
 
 
-const MOCK_FEEDS: Feed[] = [
-  {
-    id: "gvmt.rcks",
-    title: "The Truth",
-    events: [
-      {
-        id: 1,
-        at: "1986-12-21",
-        title: "Florida woman found drunk in refrigirator",
-        description: "Authorities are confused... but not surprised.",
-      },
-      {
-        id: 2,
-        at: "2017-05-06",
-        title: "Refrigirator prices crush real estate market",
-        description: "Boomers are to blame again.",
-      },
-    ],
-  },
-  {
-    id: "itsfreezinghere",
-    title: "North Pole Digest",
-    events: [
-      {
-        id: 3,
-        at: "1200-09-10",
-        title: "Nothing happened",
-      },
-      {
-        id: 4,
-        at: "1201-03-01",
-        title: "One penguin fell in a funny way",
-        description: "But nobody noticed.",
-      },
-      {
-        id: 5,
-        at: "1761-12-10",
-        title: "Nothing happened in a while",
-      },
-    ],
-  },
-]
-
 export class FeedService {
   feedsCache: { [id: string]: Feed } = {}
+  headsCache?: FeedHead[] = undefined
 
   constructor() {
     makeAutoObservable(this)
   }
 
   async listFeeds(): Promise<FeedHead[]> {
-    //---------------------------------------------------------
-    await sleep(200)
-    const response = await fetch("/api/source_ids", {
-      method: 'GET',
-    });
-    if (!response.ok) {
-      throw new Error();
+    if (!this.headsCache) {
+      const response = await fetch("http://localhost/api/source_ids", {
+        method: 'GET',
+      });
+      if (!response.ok) {
+        throw new Error();
+      }
+      this.headsCache = await response.json()
     }
-    if (response.body !== null) {
-      let result = response.json()
-      console.log(Date.now(), "timmyb32rQQQ:listFeeds:", result)
-      return result
-    }
-    //---------------------------------------------------------
+    return this.headsCache
   }
 
   async getFeedById(id: string): Promise<Feed> {
@@ -76,20 +30,14 @@ export class FeedService {
       return null
     }
     if (!this.feedsCache[id]) {
-      await sleep(200)
-      //---------------------------------------------------------
-      const response = await fetch(`/api/source_id/${id}`, {
+      const response = await fetch(`http://localhost/api/source_id/${id}`, {
         method: 'GET',
+        credentials: "omit",
       });
       if (!response.ok) {
         throw new Error();
       }
-      if (response.body !== null) {
-        let result = response.json()
-        console.log(Date.now(), "timmyb32rQQQ:getFeedById:", result)
-        return result
-      }
-      //---------------------------------------------------------
+      this.feedsCache[id] = await response.json()
     }
 
     return this.feedsCache[id]
